930dd862a9e4a585ac2be208f0b645f49f22cbe4
Vinay Shenoy <vinay@obvious.in>
2020-11-09 15:30:19 +0530

    WIP

--- router/src/main/java/org/simple/clinic/router/screen/FullScreenKey.kt
+++ router/src/main/java/org/simple/clinic/router/screen/FullScreenKey.kt
@@ -3,11 +3,7 @@ package org.simple.clinic.router.screen
 import android.os.Parcelable
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import com.zhuinden.simplestack.ServiceBinder
 import com.zhuinden.simplestackextensions.fragments.DefaultFragmentKey
-import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
-import com.zhuinden.simplestackextensions.servicesktx.add
-import org.simple.clinic.navigation.BackPressHandler
 import org.simple.clinic.router.ScreenWrapperFragment
 
 /**
@@ -17,7 +13,7 @@ import org.simple.clinic.router.ScreenWrapperFragment
  * Note: use AutoValue or otherwise ensure equals() is overridden.
  * Screen routing is skipped if an outgoing key and an incoming key are equal.
  */
-abstract class FullScreenKey : Parcelable, DefaultFragmentKey(), DefaultServiceProvider.HasServices {
+abstract class FullScreenKey : Parcelable, DefaultFragmentKey() {
 
   abstract val analyticsName: String
 
@@ -25,10 +21,4 @@ abstract class FullScreenKey : Parcelable, DefaultFragmentKey(), DefaultServiceP
   abstract fun layoutRes(): Int
 
   override fun instantiateFragment(): Fragment = ScreenWrapperFragment.create(this)
-
-  override fun getScopeTag() = ""
-
-  override fun bindServices(serviceBinder: ServiceBinder) {
-    serviceBinder.add(serviceBinder.lookupService<BackPressHandler>("backpress"))
-  }
 }
