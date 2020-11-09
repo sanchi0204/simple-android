3bc7f3822a976f5a904beed54f096f86f2e8d54b
Vinay Shenoy <vinay@obvious.in>
2020-11-09 11:51:56 +0530

    Move setting up of back press handling to the `FullScreenKey`
    This will ensure all screens will be able to opt into the back press mechanism.

--- app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenKey.kt
+++ app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenKey.kt
@@ -1,30 +1,18 @@
 package org.simple.clinic.login.pin
 
 import android.os.Parcelable
-import com.zhuinden.simplestack.ServiceBinder
-import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
-import com.zhuinden.simplestackextensions.servicesktx.add
 import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.R
-import org.simple.clinic.navigation.BackPressHandler
 import org.simple.clinic.router.screen.FullScreenKey
 
 // TODO: Remove added otp property
 // Temporarily added to stop the current login flow from breaking
 @Parcelize
-data class LoginPinScreenKey(val otp: String = "") : FullScreenKey(), Parcelable, DefaultServiceProvider.HasServices {
+data class LoginPinScreenKey(val otp: String = "") : FullScreenKey(), Parcelable {
 
   @IgnoredOnParcel
   override val analyticsName = "Login PIN Entry"
 
   override fun layoutRes() = R.layout.screen_login_pin
-
-  override fun getScopeTag(): String {
-    return ""
-  }
-
-  override fun bindServices(serviceBinder: ServiceBinder) {
-    serviceBinder.add(serviceBinder.lookupService<BackPressHandler>("backpress"))
-  }
 }
--- router/src/main/java/org/simple/clinic/router/screen/FullScreenKey.kt
+++ router/src/main/java/org/simple/clinic/router/screen/FullScreenKey.kt
@@ -3,7 +3,11 @@ package org.simple.clinic.router.screen
 import android.os.Parcelable
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import com.zhuinden.simplestack.ServiceBinder
 import com.zhuinden.simplestackextensions.fragments.DefaultFragmentKey
+import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
+import com.zhuinden.simplestackextensions.servicesktx.add
+import org.simple.clinic.navigation.BackPressHandler
 import org.simple.clinic.router.ScreenWrapperFragment
 
 /**
@@ -13,7 +17,7 @@ import org.simple.clinic.router.ScreenWrapperFragment
  * Note: use AutoValue or otherwise ensure equals() is overridden.
  * Screen routing is skipped if an outgoing key and an incoming key are equal.
  */
-abstract class FullScreenKey : Parcelable, DefaultFragmentKey() {
+abstract class FullScreenKey : Parcelable, DefaultFragmentKey(), DefaultServiceProvider.HasServices {
 
   abstract val analyticsName: String
 
@@ -21,4 +25,10 @@ abstract class FullScreenKey : Parcelable, DefaultFragmentKey() {
   abstract fun layoutRes(): Int
 
   override fun instantiateFragment(): Fragment = ScreenWrapperFragment.create(this)
+
+  override fun getScopeTag() = ""
+
+  override fun bindServices(serviceBinder: ServiceBinder) {
+    serviceBinder.add(serviceBinder.lookupService<BackPressHandler>("backpress"))
+  }
 }
